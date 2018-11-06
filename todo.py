935e7e59b94ae99dba2dacdef44dea8c7f583a9a
Jerinca <jerinca@live.nl>
2018-11-06 16:11:40 +0100

    voorbeeld opzet

---
+++ todo.py
@@ -0,0 +1,20 @@
+# #TODO
+#
+# - coordinatensystmeem
+# - classes aanmaken
+# -
+# - grid aanmaken (seaborn en turtle)
+# - numpy panda?
+# - constraints wiskundig formuleren (poging)
+# - presententatie aanpassen
+#         # Probleemstelling correct formuleren
+#         # methode / plan van aanpak
+#         # welke packages?
+# - I  (Infrastructure)and  D  (Datastructure):  Infrastructure  is  the  general
+# structure  and  order  of  the  project.  Think  of  things  like  setting  up  Git,
+# processing  the  data  given  in  the  case,  visualisation,  etc.For  datastructure
+# we  expect  the  creation  of  a  programmed  structure  that  can  hold  the  information
+# that  is  required  for  representing  a  state  of  the  case’s  problem.
+# It  is  also  essential  that  this  datastructure  can  be  manipulated/the  state  altered.
+# - copy opzet van voorbeeld page (SCORE 1 POINT)                                                   DONE!!!
+# - fill in read me
