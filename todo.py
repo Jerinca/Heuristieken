c2ab509cf8bcfd47eb45b9915d54652754804971
yangyangto <yangyang.yyt@gmail.com>
2018-11-13 12:34:30 +0100

    new todo list

--- todo.py
+++ todo.py
@@ -1,20 +1,22 @@
 # #TODO
 #
-# - coordinatensystmeem
-# - classes aanmaken
-# -
-# - grid aanmaken (seaborn en turtle)
-# - numpy panda?
 # - constraints wiskundig formuleren (poging)
-# - presententatie aanpassen
+# - presententatie (wetenschappelijk)
 #         # Probleemstelling correct formuleren
-#         # methode / plan van aanpak
-#         # welke packages?
+
 # - I  (Infrastructure)and  D  (Datastructure):  Infrastructure  is  the  general
 # structure  and  order  of  the  project.  Think  of  things  like  setting  up  Git,
 # processing  the  data  given  in  the  case,  visualisation,  etc.For  datastructure
 # we  expect  the  creation  of  a  programmed  structure  that  can  hold  the  information
 # that  is  required  for  representing  a  state  of  the  case’s  problem.
 # It  is  also  essential  that  this  datastructure  can  be  manipulated/the  state  altered.
-# - copy opzet van voorbeeld page (SCORE 1 POINT)                                                   DONE!!!
+
+# - git ignore invullen
 # - fill in read me
+# - classes importeren
+# - requirements.txt invullen (heb je matlab packages voor(?))
+# - state space berekenen
+# - contraints niet hardcoden, maar inlezen (excel bijv.)
+
+
+# - Seaborn/ turtle
