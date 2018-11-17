59c8367709a9fa64ab3469a5bec4c8bd98893c06
JRMfer <43999123+JRMfer@users.noreply.github.com>
2018-11-17 23:25:40 +0100

    main code

--- AmstelHaege/code/main.py
+++ AmstelHaege/code/main.py
@@ -2,14 +2,14 @@ import csv
 import pandas as pd
 
 
-INPUT_CSV = "https://github.com/Jerinca/Heuristieken/blob/master/AmstelHaege/data/Gegevenshuizen.csv"
+INPUT_CSV = "https://raw.githubusercontent.com/Jerinca/Heuristieken/master/AmstelHaege/data/Gegevenshuizen.csv"
 
 def csv_reader(filename):
     """
     Loads csv file as data frame
     """
 
-    data = pd.read_csv(filename)
+    data = pd.read_csv(filename, index_col=0)
     return data
 
 if __name__ == "__main__":
