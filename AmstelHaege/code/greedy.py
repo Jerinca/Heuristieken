6843f1398a3b07538823f8816fafee776e9ae6b7
JRMfer <43999123+JRMfer@users.noreply.github.com>
2018-11-29 16:59:49 +0100

    greedy

--- AmstelHaege/code/greedy.py
+++ AmstelHaege/code/greedy.py
@@ -11,6 +11,7 @@ import numpy as np
 from random import randint
 import matplotlib.pyplot as plt
 from class_house import House_types, House, Bungalow, Maison
+from class_area import plot_distribution
 
 TOTAL_HOUSES = [20, 40, 60]
 PERCENTAGES = [0.15, 0.35, 0.6]
@@ -49,13 +50,18 @@ def main():
 
         if len(houses_greedy) < (TOTAL_HOUSES[0] * PERCENTAGES[0]):
 
-            height_steps = np.arange(0, HEIGHT, HEIGHT / maison.length)
-            width_steps = np.arange(0, WIDTH, WIDTH / maison.width)
+            # misschien de stappen afronden: boven of beneden?
+            # height_steps = np.arange(0, HEIGHT, HEIGHT / maison.length)
+            # width_steps = np.arange(0, WIDTH, WIDTH / maison.width)
+            # print(height_steps)
+            # print(width_steps)
             houses_placed = []
-            distances = []
 
-            for y_bottom_left in height_steps:
-                for x_bottom_left in width_steps:
+            for y_bottom_left in range(0, HEIGHT, maison.length):
+                for x_bottom_left in range(0, WIDTH, maison.width):
+
+            # for y_bottom_left in height_steps:
+            #     for x_bottom_left in width_steps:
 
                     house_to_place = Maison(x_bottom_left, y_bottom_left, 0)
                     house_to_place_rect = house_to_place.rectangle()
@@ -72,23 +78,100 @@ def main():
 
                         houses_placed.append(house_to_place)
 
-            max_distances_all_houses = []
+            min_distances_houses = []
+
+            print(houses_placed)
+            for house in houses_placed:
+                # geeft de minimale afstand van het huis t.o.v de al geplaatste
+                # huizen in houses_greedy
+                distance_min = house.calculate_dist(houses_greedy)
+                min_distances_houses.append(distance_min)
+
+            # find highest value in min_distances_houses and corresponding
+            # house and append this one to houses_greedy
+            # print(min_distances_houses)
+            maxpos = min_distances_houses.index(max(min_distances_houses))
+            houses_greedy.append(houses_placed[maxpos])
+            plot_distribution(houses_greedy)
+
+        elif len(houses_greedy) < ((TOTAL_HOUSES[0] * PERCENTAGES[0]) + (TOTAL_HOUSES[0] * PERCENTAGES[1])):
+
+            height_steps = np.arange(0, HEIGHT, HEIGHT / bungalow.length)
+            width_steps = np.arange(0, WIDTH, WIDTH / bungalow.width)
+            houses_placed = []
+
+            for y_bottom_left in height_steps:
+                for x_bottom_left in width_steps:
+
+                    house_to_place = Bungalow(x_bottom_left, y_bottom_left, 0)
+                    house_to_place_rect = house_to_place.rectangle()
+                    house_to_place.get_coordinates(house_to_place_rect)
+
+                    if house_to_place.in_map():
+                        count = 0
+
+                    for house in houses_greedy:
+                        if house_to_place.intersect(house):
+                            count += 1
+
+                    if count == 0:
+
+                        houses_placed.append(house_to_place)
+
+            min_distances_houses = []
 
             for house in houses_placed:
+                # geeft de minimale afstand van het huis t.o.v de al geplaatste
+                # huizen in houses_greedy
+                distance_min = house.calculate_dist(houses_greedy)
+                min_distances_houses.append(distance_min)
 
-                distances = house.calculate_dist(houses_greedy)
-                print(distances)
-                # find shortest distance
-                distance = max(distances)
-                max_distances_all_houses.append(distance)
-
-            # find highest value in max_distances_all_houses and corresponding
+            # find highest value in min_distances_houses and corresponding
             # house and append this one to houses_greedy
+            maxpos = min_distances_houses.index(max(min_distances_houses))
+            houses_greedy.append(houses_placed[maxpos])
+            plot_distribution(houses_greedy)
 
-            ## calculate distance klopt nog niet!!!! hij geeft bij een huis op
-            ## coord (0, 0) een afstand van 360 ipv 0.
+        else:
 
+            height_steps = np.arange(0, HEIGHT, HEIGHT / house.length)
+            width_steps = np.arange(0, WIDTH, WIDTH / house.width)
+            houses_placed = []
 
+            for y_bottom_left in height_steps:
+                for x_bottom_left in width_steps:
+
+                    house_to_place = House(x_bottom_left, y_bottom_left, 0)
+                    house_to_place_rect = house_to_place.rectangle()
+                    house_to_place.get_coordinates(house_to_place_rect)
+
+                    if house_to_place.in_map():
+                        count = 0
+
+                    for house in houses_greedy:
+                        if house_to_place.intersect(house):
+                            count += 1
+
+                    if count == 0:
+
+                        houses_placed.append(house_to_place)
+
+            min_distances_houses = []
+
+            for house in houses_placed:
+                # geeft de minimale afstand van het huis t.o.v de al geplaatste
+                # huizen in houses_greedy
+                distance_min = house.calculate_dist(houses_greedy)
+                min_distances_houses.append(distance_min)
+
+            # find highest value in min_distances_houses and corresponding
+            # house and append this one to houses_greedy
+            maxpos = min_distances_houses.index(max(min_distances_houses))
+            houses_greedy.append(houses_placed[maxpos])
+            plot_distribution(houses_greedy)
+
+    print(houses_greedy)
+    plot_distribution(houses_greedy)
 
 
 if __name__ == "__main__":
