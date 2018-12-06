2433672af12f37c02282fbd7cdd93f7fed17fbb7
yangyangto <43998027+yangyangto@users.noreply.github.com>
2018-12-06 12:58:27 +0100

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,22 +1,18 @@
 # #TODO
 #
-# - constraints wiskundig formuleren (poging)
-# - presententatie (wetenschappelijk)
-#         # Probleemstelling correct formuleren
+# -	Waarde van de greedies 
+# -	Random hillclimber aanpassen
+# -	Hill climber op de greedies toepassen
+# -	Score in titel grafiek zetten
+# -	Pijnpunten van case beschrijven 
+# -	Hillclimber niet per huis, maar random huis kiezen, maar wel systematisch alles langsgaan.
+# -	Verloop plotten
+# -	Read me in algoritme
+# -	Histogram/ vergelijking algoritme 
 
-# - I  (Infrastructure)and  D  (Datastructure):  Infrastructure  is  the  general
-# structure  and  order  of  the  project.  Think  of  things  like  setting  up  Git,
-# processing  the  data  given  in  the  case,  visualisation,  etc.For  datastructure
-# we  expect  the  creation  of  a  programmed  structure  that  can  hold  the  information
-# that  is  required  for  representing  a  state  of  the  case’s  problem.
-# It  is  also  essential  that  this  datastructure  can  be  manipulated/the  state  altered.
 
 # - git ignore invullen
-# - fill in read me
-# - classes importeren
+# - read me aanpassen
 # - requirements.txt invullen (heb je matlab packages voor(?))
-# - state space berekenen
 # - contraints niet hardcoden, maar inlezen (excel bijv.)
 
-
-# - Seaborn/ turtle
