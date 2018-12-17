1150b847f6c9fa5bfaddc01e0cf5e3e440e5af47
Jerinca <jerinca@live.nl>
2018-12-17 18:33:03 +0100

    comments class area

--- AmstelHaege/code/class_area.py
+++ AmstelHaege/code/class_area.py
@@ -3,14 +3,9 @@ import pandas as pd
 from random import randint
 import matplotlib.pyplot as plt
 from class_house import House_types, House, Bungalow, Maison
-# import os
-# import sys
 
-
+# use this file as input
 INPUT_CSV = "https://raw.githubusercontent.com/Jerinca/Heuristieken/master/AmstelHaege/data/Gegevenshuizen.csv"
-# TOTAL_HOUSES = [20, 40, 60]
-# WIDTH = 320
-# HEIGHT = 360
 
 class Area(object):
     """
@@ -19,6 +14,7 @@ class Area(object):
     and set as class attributes.
     """
 
+    # set w, h, and proportion of houses
     height = 360
     width = 320
     portions = [0.6, 0.25, 0.15]
@@ -34,9 +30,15 @@ class Area(object):
         self.houses_placed = []
 
     def __eq__(self, other):
+        """
+        Makes sure you can compare objects
+        """
         return self.__dict__ == other.__dict__
 
     def __hash__(self):
+        """
+        Hash function for the houses that need to be placed
+        """
         return hash(self.value, self.amount_houses, self.houses_placed)
 
 
@@ -44,7 +46,6 @@ class Area(object):
         """
         Loads csv file as data frame
         """
-
         data = pd.read_csv(filename, index_col=0, sep=";", decimal=",")
         return data
 
@@ -52,53 +53,65 @@ class Area(object):
         """
         Transforms data frame to dictionary
         """
-
         dict = df.to_dict()
         return dict
 
     def place_house(self, new_house):
         """
-        Allen handig voor random plaatsen of iedergeval voor
-        als er geen  verder vergelijkingen nodig zijn dan die al in de functie
-        worden beschreven en de huizen direct geplaatst kunnen worden.
+        Function that places random houses
         """
+
+        # if there is a new house in the map set counter 0
         if new_house.in_map():
             count == 0
 
+        # for every house placed
         for house in self.houses_placed:
+            # if new house intersects with existing house counter + 1
             if new_house.intersect(house):
                 count += 1
 
+        # if no intersection, append new house
         if count == 0:
             self.houses_placed.append(new_house)
 
     def place_houses(self):
+        """
+        Function that can place houses
+        But makes sure they dont overlap
+        And stay in the map
+        """
 
-        # list_houses = []
-
+        # when not yet the amout of houses needed for map
         while len(self.houses_placed) < self.amount_houses:
+
+             # create coordinates of new House 
              if len(self.houses_placed) < (self.amount_houses * self.portions[0]):
                  x = randint(0, self.width)
                  y = randint(0, self.height)
                  new_house = House(x, y, 0)
                  house_rect = new_house.rectangle()
+
+                 # with getting the coordinates make sure there is no overlap
                  new_house.get_coordinates(house_rect)
                  if new_house.in_map():
                      count = 0
-
                      for house in self.houses_placed:
                          if new_house.intersect(house):
                              count += 1
 
+                     # if no overlap append new house
                      if count == 0:
-
                          self.houses_placed.append(new_house)
 
+             # create coordinates of new Bungalow
              elif len(self.houses_placed) < ((self.amount_houses * self.portions[0]) + (self.amount_houses * self.portions[1])):
                  x = randint(0, self.width)
                  y = randint(0, self.height)
                  new_house = Bungalow(x, y, 0)
                  house_rect = new_house.rectangle()
+
+                 # with getting the coordinates make sure there is no overlap
                  new_house.get_coordinates(house_rect)
                  if new_house.in_map():
                      count = 0
@@ -107,15 +120,18 @@ class Area(object):
                          if new_house.intersect(house):
                              count += 1
 
+                     # if no overlap append new house
                      if count == 0:
-
                          self.houses_placed.append(new_house)
 
+            # create coordinates of new Maison
              else:
                  x = randint(0, self.width)
                  y = randint(0, self.height)
                  new_house = Maison(x, y, 0)
                  house_rect = new_house.rectangle()
+
+                 # with getting the coordinates make sure there is no overlap
                  new_house.get_coordinates(house_rect)
                  if new_house.in_map():
                      count = 0
@@ -124,36 +140,49 @@ class Area(object):
                          if new_house.intersect(house):
                              count += 1
 
+                     # if no overlap append new house
                      if count == 0:
-
                          self.houses_placed.append(new_house)
 
-        # self.houses_placed[7].calculate_dist(self.houses_placed)
-    # return list_houses
 
     def remove_house(self, house):
+        """ 
+        A function that makes it possible to remove a house that has been placed
+        """
         self.houses_placed.remove(house)
 
 
     def calculate_totalvalue(self):
+        """
+        This function calculates the value of a map
+        """
 
         counter = 0
         all_values = []
 
+        # for every house that is placed
         for house in self.houses_placed:
+
+            # calculate distance
             trigger = house.calculate_dist(self.houses_placed)
-            # print(trigger)
             counter += 1
+
+            # calculate value for that house
             house.calculate_value(trigger)
-            # value_house = house.value
+
+            # append all values
             all_values.append(house.value)
 
+        # sum up
         self.value = sum(all_values)
-        # print(sum_values, "TOTALE DOEKOES!!!--------")
-        # return sum_values
 
     def plot_distribution(self):
+        """
+        This function is going to plot a distribution
+        With the values
+        """
 
+        # set attributes
         fig = plt.figure(figsize=(6,8))
         ax = fig.add_subplot(111)
         ax.set_facecolor("green")
@@ -164,73 +193,18 @@ class Area(object):
             ax.add_patch(house_rec)
 
         plt.title(f'Map AmstelHaege: {round(self.value)}')
-        # plt.axis('scaled')
-        # plt.yscale("linear")
-        # plt.xscale("linear")
-
         plt.grid()
-        # plt.show()
 
         return fig
 
     def move_house(self, index, new_x, new_y):
+        """
+        This function makes sure you can move a house in the map
+        from old x & y coordinates to new ones
+        """
+
+        # set new x and new y
         self.houses_placed[index].x = new_x
         self.houses_placed[index].y = new_y
         rect = self.houses_placed[index].rectangle()
         self.houses_placed[index].get_coordinates(rect)
-
-        # move_house(old_list, index, randint(0, WIDTH), randint(0, HEIGHT))
-
-    def legit_placement(self, new_house):
-        house_rect = new_house.rectangle()
-        new_house.get_coordinates(house_rect)
-        # print(new_house.x)
-        # print(new_house.y)
-
-        if not new_house.in_map():
-            count += 1
-
-            for house in self.houses_placed:
-                if not house == new_house:
-                    if new_house.intersect(house):
-                        return False
-
-            if count == 0:
-                return True
-
-        return False
-
-if __name__ == "__main__":
-
-    amstelhaege = Area(20)
-    amstelhaege.place_houses()
-    print(len(amstelhaege.houses_placed))
-    amstelhaege.calculate_totalvalue()
-    amstelhaege.plot_distribution()
-    print(amstelhaege.value)
-
-
-    # data = csv_reader(INPUT_CSV)
-    # # print(data)
-    # test = data["Percentage"][0]
-    # # print(type(test))
-    # dict = df_to_dict(data)
-    # # print(dict)
-    # # print(type(dict["Percentage"]["Eensgezins"]))
-    #
-    # list_houses = place_houses(TOTAL_HOUSES, data["Percentage"])
-    # # list_houses_new = move_house(list_houses, 1, randint(0, WIDTH), randint(0, HEIGHT))
-    # list_houses_new = move_house(list_houses, 1, list_houses[2].x + 1, list_houses[2].y + 1)
-    # # plot_distribution(list_houses_new)
-    #
-    # while not legit_placement(list_houses_new[1], list_houses_new):
-    #     list_houses_new = move_house(list_houses_new, 1, randint(0, WIDTH), randint(0, HEIGHT))
-    #     print("here")
-    #
-    # print("not here")
-    #
-    # # print(list_houses)
-    # # print(len(list_houses))
-    # # plot_distribution(list_houses)
-    #
-    # # if not house == new_house werkt niet
